5e053c55b4d78ad6f645b566ebebef41fc180884
BirdyMaster <domassirbike@gmail.com>
2022-10-17 23:44:04 +0200

    Add squares with colors -------------------------------------
    -------------------------------------

--- java.js
+++ java.js
@@ -1,3 +1,19 @@
 
 const input = document.querySelectorAll('input')
+const godFatherBox = document.querySelector('.godFatherBox')
+const colors = [
+    {
+        color1: 'red',
+        color2: 'orange',
+        color3: 'blue',
+        color4: 'green'
+    },
+    {
+        color1: 'orange',
+        color2: 'pink',
+        color3: 'red',
+        color4: 'blue'
+    },
+
+]
 
